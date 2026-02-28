7ca51d4ca0b2476491b115755bc1cc4c2ca7a177
Luis-Alberto-Valdes <luisalbertovaldes.dev@gmail.com>
2026-02-28 08:41:15 -0500

    v1.2.0 new email service

--- src/utils/emails.js
+++ src/utils/emails.js
@@ -1,49 +1,67 @@
 // Import the Nodemailer library
 import nodemailer from 'nodemailer'
 import ejs from 'ejs'
+import { google } from 'googleapis'
 
-const KEY = process.env.GMAIL_KEY
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
+)
 
+oAuth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN
+})
 export async function sendEmail (mailOptions) {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    ipv4: true,
-    auth: {
-      user: 'morningdigest.news@gmail.com',
-      pass: KEY,
-    }
-  })
+  try {
+    const accessToken = await oAuth2Client.getAccessToken()
 
-  await transporter.sendMail(mailOptions)
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken
+      }
+    })
+
+    await transporter.sendMail(mailOptions)
+  } catch (error) {
+    console.error('Error sending emails:', error)
+  }
 }
 
 export async function sendMultipleEmail (mailOptions) {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'morningdigest.news@gmail.com',
-      pass: KEY,
-    },
-    maxConnections: 5,
-    maxMessages: Infinity
-  })
-
-  const emailPromises = mailOptions.map(async recipient => {
-    console.log(recipient.value.notices)
-    const html = await ejs.renderFile('./src/views/notices-email.ejs', { notice: recipient.value.notices })
-
-    return transporter.sendMail({
-      from: 'morningdigest.news@gmail.com',
-      to: recipient.value.emails,
-      subject: 'Notices of the day',
-      html
-    })
-  })
   try {
+    const accessToken = await oAuth2Client.getAccessToken()
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken
+      },
+      maxConnections: 5,
+      maxMessages: 100
+    })
+
+    const emailPromises = mailOptions.map(async recipient => {
+      const html = await ejs.renderFile('./src/views/notices-email.ejs', { notice: recipient.value.notices })
+
+      return transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: recipient.value.emails,
+        subject: 'Notices of the day',
+        html
+      })
+    })
+
     await Promise.all(emailPromises)
   } catch (error) {
     console.error('Error sending emails:', error)
